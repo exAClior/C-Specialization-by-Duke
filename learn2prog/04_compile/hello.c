536585deb66e887b05c3d8b65c8c4cc163be27f6
exAClior <yushengzhao2020@outlook.com>
2022-07-24 15:01:16 +0800

    do ex04

--- learn2prog/04_compile/hello.c
+++ learn2prog/04_compile/hello.c
@@ -1,10 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include "stdio.h"
+#include "stdlib.h"
 
-#define EXIT_SUCCESS 0
-
-int main()
-{
+int main() {
   printf("Hello World\n");
   return EXIT_SUCCESS;
 }
