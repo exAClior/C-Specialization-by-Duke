3c85cefd411bf89fd9473096b6788bab29f861d0
exAClior <yushengzhao2020@outlook.com>
2022-08-06 15:23:31 +0800

    finish 18

--- learn2prog/18_reverse_str/reverse.c
+++ learn2prog/18_reverse_str/reverse.c
@@ -3,32 +3,39 @@
 #include <string.h>
 
 void reverse(char *str) {
-  //WRITE ME!
-  //  char check[]=*str;
-  //printf(
-  //strrev(str);
-  if(str==NULL)
-   return ;
-  size_t i;
-  size_t n=strlen(str);
-  char temp;
-  //t=str;temp=str;
-  //  for(i=0;i<n;++i)
-    // temp++;
-  i=0;
-    while(i<n/2)
-    {
-      /* ch=*temp;
-      *temp=*t;
-      *t=ch;
-      t++;
-      temp--;*/
-      temp=str[i];
-      str[i]=str[n-1-i];
-      str[n-i-1]=temp;
-      ++i;
-    }
-    return;
+  size_t size = strlen(str);
+  char tmp;
+  for (size_t i = 0; i < size / 2; i++) {
+    tmp = str[i];
+    str[i] = str[size - 1 - i];
+    str[size - 1 - i] = tmp;
+  }
+  return;
+  // WRITE ME!
+  //   char check[]=*str;
+  // printf(
+  // strrev(str);
+  /* if (str == NULL) */
+  /*   return; */
+  /* size_t i; */
+  /* size_t n = strlen(str); */
+  /* char temp; */
+  /* // t=str;temp=str; */
+  /* //   for(i=0;i<n;++i) */
+  /* //  temp++; */
+  /* i = 0; */
+  /* while (i < n / 2) { */
+  /*   /\* ch=*temp; */
+  /*   *temp=*t; */
+  /*   *t=ch; */
+  /*   t++; */
+  /*   temp--;*\/ */
+  /*   temp = str[i]; */
+  /*   str[i] = str[n - 1 - i]; */
+  /*   str[n - i - 1] = temp; */
+  /*   ++i; */
+  /* } */
+  /* return; */
 }
 
 int main(void) {
@@ -38,7 +45,8 @@ int main(void) {
   char str3[] = "Captain's log, Stardate 42523.7";
   char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
-  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! "
+                "There may be no tomorrow!";
   char *array[] = {str0, str1, str2, str3, str4, str5, str6};
   for (int i = 0; i < 7; i++) {
     reverse(array[i]);
